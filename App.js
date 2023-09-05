7c2bcd94601e4f4993a2fe44166ed03d3b229d19
Lauracandoit <132577507+Lauracandoit@users.noreply.github.com>
2023-09-05 15:18:05 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import WeatherApp from './Components/WeatherApp/WeatherApp';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp/>
+      
+    </div>
+  );
+}
+
+export default App;
+
+
+// https://www.youtube.com/watch?v=7JqdjWB88Kk
